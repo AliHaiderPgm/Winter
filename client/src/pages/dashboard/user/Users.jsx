420fb52a56abdf7257ab25af2502f80b593bced0
Ali Haider <alihaider203472@gmail.com>
2023-09-21 23:01:06 +0500

    search user by name and email in antd table

--- client/src/pages/dashboard/user/Users.jsx
+++ client/src/pages/dashboard/user/Users.jsx
@@ -1,19 +1,117 @@
-import { Button, Space, Table } from "antd"
+import { Button, Input, Space, Table } from "antd"
 import { userData } from "./TableData"
-import { DeleteOutlined } from "@ant-design/icons"
+import { DeleteOutlined, SearchOutlined } from "@ant-design/icons"
+import { useRef, useState } from "react"
+import Highlighter from "react-highlight-words"
 
 const Users = () => {
+	const [searchText, setSearchText] = useState("")
+	const [searchedColumn, setSearchedColumn] = useState("")
+	const searchInput = useRef(null)
+	const handleSearch = (selectedKeys, confirm, dataIndex) => {
+		confirm()
+		setSearchText(selectedKeys[0])
+		setSearchedColumn(dataIndex)
+	}
+	const getColumnSearchProps = (dataIndex) => ({
+		filterDropdown: ({
+			setSelectedKeys,
+			selectedKeys,
+			confirm,
+			clearFilters,
+			close,
+		}) => (
+			<div
+				style={{
+					padding: 8,
+				}}
+				onKeyDown={(e) => e.stopPropagation()}
+			>
+				<Input
+					ref={searchInput}
+					placeholder={`Search by ${dataIndex}`}
+					value={selectedKeys[0]}
+					onChange={(e) =>
+						setSelectedKeys(e.target.value ? [e.target.value] : [])
+					}
+					onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+					style={{
+						marginBottom: 8,
+						display: "block",
+					}}
+				/>
+				<Space>
+					<Button
+						type="text"
+						size="middle"
+						onClick={() => {
+							setSearchText(selectedKeys[0])
+							setSearchedColumn(dataIndex)
+							confirm({
+								closeDropdown: true,
+							})
+						}}
+					>
+						Filter
+					</Button>
+					<Button
+						size="middle"
+						type="primary"
+						onClick={() => {
+							clearFilters()
+							confirm({
+								closeDropdown: true,
+							})
+							setSearchText("")
+							setSearchedColumn(dataIndex)
+						}}
+					>
+						Reset
+					</Button>
+				</Space>
+			</div>
+		),
+		filterIcon: (filtered) => (
+			<SearchOutlined
+				style={{
+					color: filtered ? "#1677ff" : undefined,
+				}}
+			/>
+		),
+		onFilter: (value, record) =>
+			record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+		onFilterDropdownOpenChange: (visible) => {
+			if (visible) {
+				setTimeout(() => searchInput.current?.select(), 100)
+			}
+		},
+		render: (text) =>
+			searchedColumn === dataIndex ? (
+				<Highlighter
+					highlightStyle={{
+						backgroundColor: "#ffc069",
+						padding: 0,
+					}}
+					searchWords={[searchText]}
+					autoEscape
+					textToHighlight={text ? text.toString() : ""}
+				/>
+			) : (
+				text
+			),
+	})
 	const columns = [
 		{
 			title: "Name",
 			dataIndex: "name",
 			key: "name",
-			// render: (text) => <a>{text}</a>,
+			...getColumnSearchProps("name"),
 		},
 		{
 			title: "Email",
 			dataIndex: "email",
 			key: "email",
+			...getColumnSearchProps("email"),
 		},
 		{
 			title: "Role",
@@ -31,26 +129,6 @@ const Users = () => {
 			],
 			onFilter: (value, record) => record.role.indexOf(value) === 0,
 		},
-		// {
-		// 	title: "Tags",
-		// 	key: "tags",
-		// 	dataIndex: "tags",
-		// 	render: (_, { tags }) => (
-		// 		<>
-		// 			{tags.map((tag) => {
-		// 				let color = tag.length > 5 ? "geekblue" : "green"
-		// 				if (tag === "loser") {
-		// 					color = "volcano"
-		// 				}
-		// 				return (
-		// 					<Tag color={color} key={tag}>
-		// 						{tag.toUpperCase()}
-		// 					</Tag>
-		// 				)
-		// 			})}
-		// 		</>
-		// 	),
-		// },
 		{
 			title: "Action",
 			key: "action",
