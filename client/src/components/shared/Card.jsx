63bc3841d9ce019739af5a38aa49ccb36330f924
Ali Haider <alihaider203472@gmail.com>
2023-10-16 23:27:18 +0500

    filters are being added

--- client/src/components/shared/Card.jsx
+++ client/src/components/shared/Card.jsx
@@ -1,10 +1,24 @@
 import { Card } from "antd"
+import { useState } from "react";
 const { Meta } = Card
 const CustomCard = ({ data }) => {
+	const [isLoading, setIsLoading] = useState(true);
+	const [isError, setIsError] = useState(false);
+
+	const handleLoad = () => {
+		setIsLoading(false);
+		setIsError(false);
+	};
+
+	const handleError = () => {
+		setIsLoading(false);
+		setIsError(true);
+	};
 	return (
 		<Card
 			hoverable
-			cover={<img alt="Product Image" src={data.images[0]} />}
+			loading={isLoading}
+			cover={<img alt="Product Image" src={data.images[0]} style={{ height: "200px", objectFit: "cover" }} onLoad={handleLoad} onError={handleError} />}
 		>
 			<Meta title={data.name} description={`$${data.price}`} />
 		</Card>
--- client/src/pages/frontend/Catalog.jsx
+++ client/src/pages/frontend/Catalog.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useRef, useState } from "react"
 import { useParams } from "react-router-dom"
 import { useProduct } from "../../context/ProductContext"
-import { message } from "antd"
+import { Button, Checkbox, Collapse, message } from "antd"
 import Loader from "../../components/shared/Loader"
 import BnbCard from "../../components/shared/BnbCard"
+import { FilterOutlined } from "@ant-design/icons"
 
 const Catalog = () => {
     const { type } = useParams()
@@ -12,18 +13,9 @@ const Catalog = () => {
     const [products, setProducts] = useState([])
     const [page, setPage] = useState(1)
     const log = useRef(true)
-
-    const getScrollProducts = async () => {
-        try {
-            setLoading(true)
-            const res = await GetCustomizedProducts("shoefor", type, page)
-            setProducts(prev => [...prev, ...res])
-        } catch (error) {
-            message.error("Something went wrong!")
-        } finally {
-            setLoading(false)
-        }
-    }
+    const loader = useRef(true)
+    const [isResEmpty, setIsResEmpty] = useState(false)
+    const [checkedVals, setCheckedVals] = useState([])
 
     const handleScroll = () => {
         if (document.documentElement.scrollTop + window.innerHeight + 200 >= document.documentElement.scrollHeight) {
@@ -32,10 +24,24 @@ const Catalog = () => {
         }
     }
 
+    const getScrollProducts = async () => {
+        try {
+            loader.current ? setLoading(true) : setLoading(false)
+            const res = await GetCustomizedProducts("shoefor", type, page)
+            res.length === 0 ? setIsResEmpty(true) : setIsResEmpty(false)
+            setProducts(prev => [...prev, ...res])
+        } catch (error) {
+            message.error("Something went wrong!")
+        } finally {
+            setLoading(false)
+        }
+    }
+
     useEffect(() => {
-        if (log.current) {
+        if (log.current && !isResEmpty) {
             getScrollProducts()
             log.current = false
+            loader.current = false
         }
     }, [page])
 
@@ -44,24 +50,112 @@ const Catalog = () => {
         return () => { window.removeEventListener("scroll", handleScroll) }
     }, [])
 
-    return (
-        <div className="row p-4">
-            <div className="col-3">
-                <p>Hello world!</p>
-            </div>
-            <div className="col-9 d-flex">
-                <div className="row">
+    ////////Sorting//////// 
+    const sortingOptions = [
+        {
+            index: "0",
+            label: "Shop by Price",
+            options: [
+                {
+                    label: "Rs.2000 - Rs.5000",
+                    value: "2000-5000"
+                },
+                {
+                    label: "Rs.5000 - Rs.10,000",
+                    value: "5000-10000"
+                },
+                {
+                    label: "Over Rs.10,000",
+                    value: "10000-999999999"
+                },
+            ]
+        },
+        {
+            index: "1",
+            label: "Types",
+            options: [
+                {
+                    value: "Sneakers",
+                    label: "Sneakers",
+                },
+                {
+                    value: "Sportswear",
+                    label: "Sportswear",
+                },
+                {
+                    value: "Running",
+                    label: "Running",
+                },
+                {
+                    value: "Golf",
+                    label: "Golf",
+                },
+                {
+                    value: "Workout & Gym",
+                    label: "Workout & Gym",
+                },
+                {
+                    value: "Football",
+                    label: "Football",
+                },
+                {
+                    value: "Basketball",
+                    label: "Basketball",
+                },
+                {
+                    value: "LifeStyle",
+                    label: "LifeStyle",
+                },
+            ]
+        },
+    ]
+
+    const handleCheckBox = (checkedValue, index) => {
+        const newArray = [...checkedVals]
+        newArray[index] = checkedValue
+        setCheckedVals(newArray)
+    }
+
+    return <div>
+        <h1 className="px-5 py-3">Men's Shoes</h1>
+        <div className="row justify-content-center p-4 gap-5">
+            <div className="col-2">
+                <p className="fw-bold fs-5">Filter <FilterOutlined style={{ verticalAlign: "0" }} /></p>
+                <div className="dropDowns d-flex flex-column gap-2">
                     {
-                        products?.map((product, index) => {
-                            return <div className="col-3 flex-fill mb-4" key={index}>
-                                <BnbCard data={product} />
-                            </div>
+                        sortingOptions.map((option, index) => {
+                            return <Collapse
+                                key={index}
+                                items={[
+                                    {
+                                        key: index,
+                                        label: option.label,
+                                        children: <Checkbox.Group options={option.options} onChange={val => handleCheckBox(val, option.index)} />,
+                                    },
+                                ]}
+                            />
                         })
                     }
+                    <div>
+                        <Button className="btn-filled" type="primary" onClick={() => console.log(checkedVals)}>Filter</Button>
+                    </div>
                 </div>
             </div>
+            <div className="col-9 d-flex min-vh-100">
+                {
+                    loading ? <Loader /> : <div className="row">
+                        {
+                            products?.map((product, index) => {
+                                return <div className="col-3 flex-fill mb-4" key={index}>
+                                    <BnbCard data={product} />
+                                </div>
+                            })
+                        }
+                    </div>
+                }
+            </div>
         </div>
-    )
+    </div>
 }
 
 export default Catalog
