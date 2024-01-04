f21c0980cb622d5d8a13d975622cf57a023b6d1d
Ali Haider <alihaider203472@gmail.com>
2024-01-04 23:15:44 +0500

    deploy v2.1.2

---
+++ frontend/build/assets/string-convert-c60507dd.js
@@ -0,0 +1,1 @@
+var a=function(e){return e.replace(/[A-Z]/g,function(r){return"-"+r.toLowerCase()}).toLowerCase()},n=a;export{n as c};
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,14 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   },
 })
